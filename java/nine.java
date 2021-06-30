8115a8c9afcd131b7aedb885e883e535e104b3a9
Doshi Naman <78679946+DoshiNaman@users.noreply.github.com>
2021-06-30 13:57:16 +0530

    Create nine.java
    Write a static block which will be executed before main( ) method in a class

---
+++ java/nine.java
@@ -0,0 +1,13 @@
+//@naman_d0shi
+public class nine {
+    //static block
+    static{
+        System.out.println("Static block");
+    }
+    public static void main(String arg[]){
+        System.out.println("main method");
+    }
+    static{
+        System.out.println("Static2 block");
+    }
+}
