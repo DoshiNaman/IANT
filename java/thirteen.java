2036c9a3769969f210a4782840243e7edd350d11
Doshi Naman <78679946+DoshiNaman@users.noreply.github.com>
2021-07-01 14:09:44 +0530

    Create thirteen.java
    Write a program in Java to demonstrate the use of 'final' keyword in the field declaration. How it is accessed using the objects

---
+++ java/thirteen.java
@@ -0,0 +1,16 @@
+//@naman_d0shi
+
+public class thirteen {
+    final int aa=12;
+    thirteen(){
+        System.out.println(aa);
+    }
+    //thirteen(int a){
+    //    aa=a;
+    //    System.out.println(aa);
+    //}
+    public static void main(String arg[]) {
+        thirteen t=new thirteen();
+        //thirteen t1=new thirteen(23);
+    }
+}
