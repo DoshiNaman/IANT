99e436e8cb71573071c4715d34bce8c37fc4a2b0
Doshi Naman <78679946+DoshiNaman@users.noreply.github.com>
2021-07-13 13:33:42 +0530

    Delete java/twenty/outside package directory

--- java/twenty/outside package/A.java
+++
@@ -1,7 +0,0 @@
-//package Twenty.method_over_same;
-
-public class A {
-    void show(){
-        System.out.println("Inside Class-A");
-    }
-}
--- java/twenty/outside package/example.java
+++
@@ -1,8 +0,0 @@
-//package Twenty.method_over_same;
-
-public class example {
-    public static void main(String[] args) {
-        B obj=new B();
-        obj.show();
-    }
-}
