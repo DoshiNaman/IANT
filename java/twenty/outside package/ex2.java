132b4ac998b246d278024e15f766aec9577c44ef
Doshi Naman <78679946+DoshiNaman@users.noreply.github.com>
2021-07-13 13:34:31 +0530

    Create ex2.java

---
+++ java/twenty/outside package/ex2.java
@@ -0,0 +1,8 @@
+import P1.pro;
+public class ex2 {
+    public static void main(String[] args) {
+        pro p=new pro(); 
+//        System.out.println("n= "+p.n);
+        System.out.println("n_pub="+p.n_pub);  
+    }
+}
