0a1b7ce5e8899f017dd37daddde3b43354dc9ed5
Doshi Naman <78679946+DoshiNaman@users.noreply.github.com>
2021-07-13 13:31:21 +0530

    Create ex1.java

---
+++ java/twenty/inside package/ex1.java
@@ -0,0 +1,8 @@
+//@naman_d0shi
+public class ex1 {
+    public static void main(String[] args) {
+        Pro p1=new Pro();
+        System.out.println("n="+p1.n);
+        System.out.println("n_pub="+p1.n_pub);
+    }
+}
