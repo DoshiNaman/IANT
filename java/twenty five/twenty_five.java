5fccacbc791872c034ba2a00270417e37a27e104
Doshi Naman <78679946+DoshiNaman@users.noreply.github.com>
2021-07-17 13:32:14 +0530

    Create twenty_five.java

---
+++ java/twenty five/twenty_five.java
@@ -0,0 +1,24 @@
+//@naman_d0shi
+//Write a program in Java to demonstrate multiple try block and multiple catch exception 
+public class twenty_five{
+    public static void main(String[] args) {
+        try{    
+            int a[]=new int[5];    
+            a[5]=30/0;
+            //a[7]=30;    
+           }    
+           catch(ArithmeticException e)  
+              {  
+               System.out.println("Arithmetic Exception occurs");  
+              }    
+           catch(ArrayIndexOutOfBoundsException e)  
+              {  
+               System.out.println("ArrayIndexOutOfBounds Exception occurs");  
+              }    
+           catch(Exception e)  
+              {  
+               System.out.println("Parent Exception occurs");  
+              }             
+           System.out.println("rest of the code");      
+    }
+}
